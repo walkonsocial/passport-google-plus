5e83085334477bd05fa61b39194d05f809d18962
ningsuhen <ningsuhen@gmail.com>
2015-04-01 18:13:27 +0530

    fix path to strategy

---
+++ index.js
@@ -0,0 +1,11 @@
+var strategy = require('./strategy');
+
+/**
+ * Expose `Strategy` directly from package.
+ */
+exports = module.exports = strategy;
+
+/**
+ * Export constructors.
+ */
+exports.Strategy = strategy;
